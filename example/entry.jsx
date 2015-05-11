2ba9de9ad9c41836957ac56312e47fd22d0cec32
Jonathan El-Bizri <github@elbizri.com>
2015-05-11 12:15:37 -0700

    we didnt need that!

--- example/entry.jsx
+++ example/entry.jsx
@@ -5,14 +5,4 @@ import data from './data.js'
 // expose React for debugging
 window.React = React
 
-class Name extends React.Component{
-  render () {
-    return <div>{this.props.name}</div>
-  }
-  propTypes() {
-    name: PropTypes.string.isRequired
-  }
-}
-
-
 React.render(<RandomList {...data} />, document.getElementById('app'))
